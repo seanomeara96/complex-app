7390a2f837858e2e39bea80a95eae5d30932f8fb
Sean <seanom96@gmail.com>
2020-08-11 09:32:15 +0100

    fixed name on homepage

--- client/src/components/homepage/EmptyFeed.js
+++ client/src/components/homepage/EmptyFeed.js
@@ -3,12 +3,13 @@ import Flash from "../Flash";
 import { connect } from "react-redux";
 
 const EmptyFeed = (props) => {
+  console.log();
   return (
     <div className="container py-md-5 container--narrow">
       <Flash errors={props.errors} successes={props.successes} />
       <div className="text-center">
         <h2>
-          Hello <strong>{props.user.userName}</strong>, your feed is empty.
+          Hello <strong>{props.auth.auth.username}</strong>, your feed is empty.
         </h2>
         <p className="lead text-muted">
           Your feed displays the latest posts from the people you follow. If you
@@ -22,7 +23,7 @@ const EmptyFeed = (props) => {
 };
 const mapStateToProps = (state) => {
   return {
-    user: state.auth,
+    auth: state.auth,
   };
 };
 export default connect(mapStateToProps)(EmptyFeed);
